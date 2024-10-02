4b1ff11d0f3c637340090f965213045353eb7e1d
amitesh <amiteshanand20@gmail.com>
2024-10-02 15:13:23 +0530

    Create VillaAPIController class

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,20 @@
+﻿using MagicVilla_VillaAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>
+            {
+                new Villa {Id=1,Name="Pool view" },
+                new Villa {Id=2,Name="Beach view" },
+        };
+        }
+    }
+}
