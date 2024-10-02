9ef04b470901ec22adee68dea652d775847f62f1
amitesh <amiteshanand20@gmail.com>
2024-10-02 18:07:56 +0530

    Status Code in Endpoints

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -11,15 +11,24 @@ namespace MagicVilla_VillaAPI.Controllers
     public class VillaAPIController : ControllerBase
     {
         [HttpGet]
-        public IEnumerable<VillaDTO> GetVillas()
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
         {
-            return VillaStore.VillaList;
+            return Ok(VillaStore.VillaList);
         } 
         
         [HttpGet("{id:int}")] //Method expects explicitly "id" parameter of integer type,otherwise swagger won't work
-        public VillaDTO GetVilla(int id)
+        public ActionResult<VillaDTO> GetVilla(int id)
         {
-            return VillaStore.VillaList.FirstOrDefault(x => x.Id == id);
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            var villa =  VillaStore.VillaList.FirstOrDefault(x => x.Id == id);
+            if (villa == null)
+            {
+                return NotFound();
+            }
+            return Ok(villa);   
         }
     }
 }
