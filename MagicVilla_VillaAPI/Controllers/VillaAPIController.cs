ca435820127f899c5d485d386d431ccbba012288
amitesh <amiteshanand20@gmail.com>
2025-02-02 14:57:43 +0530

    Automapper in action

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,4 +1,5 @@
-﻿using MagicVilla_VillaAPI.Data;
+﻿using AutoMapper;
+using MagicVilla_VillaAPI.Data;
 using MagicVilla_VillaAPI.Models;
 using MagicVilla_VillaAPI.Models.Dto;
 using Microsoft.AspNetCore.JsonPatch;
@@ -13,16 +14,20 @@ namespace MagicVilla_VillaAPI.Controllers
     public class VillaAPIController : ControllerBase
     {
         private readonly ApplicationDbContext _db;
-        public VillaAPIController(ApplicationDbContext db)
+        private readonly IMapper _mapper;
+        public VillaAPIController(ApplicationDbContext db,IMapper mapper)
         {
             _db = db;
+            _mapper = mapper;
         }
 
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult<IEnumerable<VillaDTO>>> GetVillas()
         {
-            return Ok(await _db.Villas.ToListAsync());
+
+            IEnumerable<Villa> villaList = await _db.Villas.ToListAsync();
+            return Ok(_mapper.Map<List<VillaDTO>>(villaList));
         } 
         
         [HttpGet("{id:int}", Name = "GetVilla")] //Method expects explicitly "id" parameter of integer type,otherwise swagger won't work
@@ -41,39 +46,29 @@ namespace MagicVilla_VillaAPI.Controllers
             {
                 return NotFound();
             }
-            return Ok(villa);   
+            return Ok(_mapper.Map<VillaDTO>(villa));   
         }
         
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status201Created)] 
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<ActionResult<VillaDTO>> CreateVilla([FromBody]VillaCreateDTO villaDTO)
+        public async Task<ActionResult<VillaDTO>> CreateVilla([FromBody]VillaCreateDTO createDTO)
         {
             //add  Custom ModelState Validation
-            if (await _db.Villas.FirstOrDefaultAsync(x => x.Name.ToLower() == villaDTO.Name.ToLower()) != null)
+            if (await _db.Villas.FirstOrDefaultAsync(x => x.Name.ToLower() == createDTO.Name.ToLower()) != null)
             {
                 ModelState.AddModelError("CustomError", "Villa already exists!");
                 return BadRequest(ModelState);
             }
 
-            if (villaDTO == null)
+            if (createDTO == null)
             {
-                return BadRequest(villaDTO);
+                return BadRequest(createDTO);
             }
+            Villa model = _mapper.Map<Villa>(createDTO);    
 
-            Villa model = new Villa()
-            {
-                Name = villaDTO.Name,
-                Occupancy = villaDTO.Occupancy,
-                Sqft = villaDTO.Sqft,
-                Rate = villaDTO.Rate,
-                ImageUrl = villaDTO.ImageUrl,
-                Amenity = villaDTO.Amenity,
-                Details = villaDTO.Details,
-            };
-
-           await _db.Villas.AddAsync(model);
+            await _db.Villas.AddAsync(model);
             await _db.SaveChangesAsync();
             return CreatedAtRoute("GetVilla",new {id = model.Id },model);   // returns URL/Route of newly created record (see "location" field of response headers on Swagger UI)
         }
@@ -103,24 +98,14 @@ namespace MagicVilla_VillaAPI.Controllers
         [HttpPut("{id:int}", Name = "UpdateVilla")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> UpdateVilla(int id, [FromBody] VillaUpdateDTO villaDTO)
+        public async Task<IActionResult> UpdateVilla(int id, [FromBody] VillaUpdateDTO updateDTO)
         {
-            if(villaDTO == null || id != villaDTO.Id)
+            if(updateDTO == null || id != updateDTO.Id)
             {
                 return BadRequest();
             }
 
-            Villa model = new Villa()
-            {
-                Name = villaDTO.Name,
-                Occupancy = villaDTO.Occupancy,
-                Sqft = villaDTO.Sqft,
-                Rate = villaDTO.Rate,
-                ImageUrl = villaDTO.ImageUrl,
-                Amenity = villaDTO.Amenity,
-                Details = villaDTO.Details,
-                Id = villaDTO.Id,
-            };
+            Villa model = _mapper.Map<Villa>(updateDTO);    
 
             _db.Villas.Update(model);
             await _db.SaveChangesAsync();
@@ -138,36 +123,16 @@ namespace MagicVilla_VillaAPI.Controllers
             }
 
             var villa = await _db.Villas.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id); //Use AsNoTracking() so that EFCore doesn't track two entities with same Id, here villa and model
-            VillaUpdateDTO villaDTO = new()
-            {
-                Name = villa.Name,
-                Occupancy = villa.Occupancy,
-                Sqft = villa.Sqft,
-                Rate = villa.Rate,
-                ImageUrl = villa.ImageUrl,
-                Amenity = villa.Amenity,
-                Details = villa.Details,
-                Id = villa.Id,
-            };
 
-       
+            VillaUpdateDTO villaDTO = _mapper.Map<VillaUpdateDTO>(villa);
+
             if (villa == null)
             {
                 return NotFound();
             }
             patchDTO.ApplyTo(villaDTO, ModelState);
 
-            Villa model = new Villa()
-            {
-                Name = villaDTO.Name,
-                Occupancy = villaDTO.Occupancy,
-                Sqft = villaDTO.Sqft,
-                Rate = villaDTO.Rate,
-                ImageUrl = villaDTO.ImageUrl,
-                Amenity = villaDTO.Amenity,
-                Details = villaDTO.Details,
-                Id = villaDTO.Id,
-            };
+            Villa model = _mapper.Map<Villa>(villaDTO);
 
             _db.Villas.Update(model);
            await _db.SaveChangesAsync();
