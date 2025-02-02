851669daeafaab8585f1e47f59e1a0da833602a3
amitesh <amiteshanand20@gmail.com>
2025-02-02 14:14:13 +0530

    Separate DTO for Create and Update

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -47,7 +47,7 @@ namespace MagicVilla_VillaAPI.Controllers
         [ProducesResponseType(StatusCodes.Status201Created)] 
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public ActionResult<VillaDTO> CreateVilla([FromBody]VillaDTO villaDTO)
+        public ActionResult<VillaDTO> CreateVilla([FromBody]VillaCreateDTO villaDTO)
         {
             //add  Custom ModelState Validation
             if (_db.Villas.FirstOrDefault(x => x.Name.ToLower() == villaDTO.Name.ToLower()) != null)
@@ -60,10 +60,6 @@ namespace MagicVilla_VillaAPI.Controllers
             {
                 return BadRequest(villaDTO);
             }
-            if(villaDTO.Id > 0)
-            {
-                return StatusCode(StatusCodes.Status500InternalServerError);
-            }
 
             Villa model = new Villa()
             {
@@ -74,12 +70,11 @@ namespace MagicVilla_VillaAPI.Controllers
                 ImageUrl = villaDTO.ImageUrl,
                 Amenity = villaDTO.Amenity,
                 Details = villaDTO.Details,
-                Id = villaDTO.Id,
             };
 
             _db.Villas.Add(model);
             _db.SaveChanges();
-            return CreatedAtRoute("GetVilla",new {id = villaDTO.Id },villaDTO);   // returns URL/Route of newly created record (see "location" field of response headers on Swagger UI)
+            return CreatedAtRoute("GetVilla",new {id = model.Id },model);   // returns URL/Route of newly created record (see "location" field of response headers on Swagger UI)
         }
 
         [HttpDelete("{id:int}", Name = "DeleteVilla")]
@@ -107,7 +102,7 @@ namespace MagicVilla_VillaAPI.Controllers
         [HttpPut("{id:int}", Name = "UpdateVilla")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public IActionResult UpdateVilla(int id, [FromBody] VillaDTO villaDTO)
+        public IActionResult UpdateVilla(int id, [FromBody] VillaUpdateDTO villaDTO)
         {
             if(villaDTO == null || id != villaDTO.Id)
             {
@@ -134,7 +129,7 @@ namespace MagicVilla_VillaAPI.Controllers
         [HttpPatch("{id:int}", Name = "UpdatePartialVilla")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public IActionResult UpdatePartialVilla(int id,JsonPatchDocument<VillaDTO> patchDTO)
+        public IActionResult UpdatePartialVilla(int id,JsonPatchDocument<VillaUpdateDTO> patchDTO)
         {
             if(patchDTO == null || id == null)
             {
@@ -142,7 +137,7 @@ namespace MagicVilla_VillaAPI.Controllers
             }
 
             var villa = _db.Villas.AsNoTracking().FirstOrDefault(x => x.Id == id); //Use AsNoTracking() so that EFCore doesn't track two entities with same Id, here villa and model
-            VillaDTO villaDTO = new()
+            VillaUpdateDTO villaDTO = new()
             {
                 Name = villa.Name,
                 Occupancy = villa.Occupancy,
