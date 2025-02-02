319f0f6667a1077968689765d2b50ea357947fa9
amitesh <amiteshanand20@gmail.com>
2025-02-02 22:45:13 +0530

    Add Villa Repository

---
+++ MagicVilla_VillaAPI/Repository/IRepository/IVillaRepository.cs
@@ -0,0 +1,14 @@
+﻿using MagicVilla_VillaAPI.Models;
+using System.Linq.Expressions;
+
+namespace MagicVilla_VillaAPI.Repository.IRepository
+{
+    public interface IVillaRepository
+    {
+        Task<List<Villa>> GetAll(Expression<Func<Villa>> filter = null);
+        Task<Villa> Get(Expression<Func<Villa>> filter = null);
+        Task Create(Villa entity);
+        Task Remove(Villa entity);
+        Task Save();
+    }
+}
