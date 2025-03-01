4409e1b823de1984db301c5faabaa3d1598acde4
amitesh <amiteshanand20@gmail.com>
2025-03-01 16:21:48 +0530

    Setup MVC Project with DTOs and API models

---
+++ MagicVilla_Utility/SD.cs
@@ -0,0 +1,13 @@
+﻿namespace MagicVilla_Utility
+{
+    public static class SD
+    {
+        public enum ApiType
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE
+        }       
+    }
+}
