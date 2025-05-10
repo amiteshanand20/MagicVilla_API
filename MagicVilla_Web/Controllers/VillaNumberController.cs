24d9e4b0c8c513c7593aca79feb55c921514692a
amitesh <amiteshanand20@gmail.com>
2025-05-10 18:03:26 +0530

    Create Villa POST

--- MagicVilla_Web/Controllers/VillaNumberController.cs
+++ MagicVilla_Web/Controllers/VillaNumberController.cs
@@ -51,11 +51,11 @@ namespace MagicVilla_Web.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> CreateVillaNumber(VillaCreateDTO model)
+        public async Task<IActionResult> CreateVillaNumber(VillaNumberCreateVM model)
         {
             if (ModelState.IsValid)
             {
-                var response = await _villaService.CreateAsync<APIResponse>(model);
+                var response = await _villaNumberService.CreateAsync<APIResponse>(model.VillaNumber);
                 if (response != null && response.IsSuccess)
                 {
                     return RedirectToAction(nameof(IndexVillaNumber));
